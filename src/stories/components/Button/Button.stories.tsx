988c50840c9cf27c4a6dded9c04cb6f51388cc59
Luke Lecheler <l.lecheler@gmail.com>
2026-01-31 13:51:26 -0600

    Project config to test tsup build

--- src/stories/components/Button/Button.stories.tsx
+++ src/stories/components/Button/Button.stories.tsx
@@ -1,5 +1,4 @@
-import * as React from "react";
-
+import React from "react";
 import preview from "../../../../.storybook/preview";
 import { Button, type ButtonShape, type ButtonSize } from "./Button";
 import { Inline } from "../../foundations/layout/Inline";
--- tsup.config.ts
+++ tsup.config.ts
@@ -1,11 +1,14 @@
 import { defineConfig } from "tsup";
 
 export default defineConfig({
-  entry: ["src/stories/index.ts"],
-  format: ["esm", "cjs"],
-  dts: true,
-  sourcemap: true,
-  clean: true,
-  outDir: "dist",
-  external: ["react", "react-dom"],
+  // entry: ["./src/stories/index.ts"],
+  // format: ["esm", "cjs"],
+  // dts: true,
+  // sourcemap: true,
+  // clean: true,
+  // outDir: "dist",
+  // external: ["react", "react-dom"],
+  // esbuildOptions(options) {
+  //   options.jsx = "preserve";
+  // },
 });
